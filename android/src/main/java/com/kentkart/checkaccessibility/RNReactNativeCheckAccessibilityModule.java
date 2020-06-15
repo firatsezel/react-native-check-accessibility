ff4bbec4cd2879326599d29576d4c683e3a6fb29
Fırat Sezel <firat.sezel@gmail.com>
2020-06-15 15:50:38 +0300

    androidx support for RN => 0.60 Versions

--- android/src/main/java/com/kentkart/checkaccessibility/RNReactNativeCheckAccessibilityModule.java
+++ android/src/main/java/com/kentkart/checkaccessibility/RNReactNativeCheckAccessibilityModule.java
@@ -3,7 +3,7 @@ package com.kentkart.checkaccessibility;
 
 
 import android.content.Context;
-import android.support.v4.view.accessibility.AccessibilityEventCompat;
+import androidx.core.view.accessibility.AccessibilityEventCompat;
 import android.view.accessibility.AccessibilityEvent;
 import com.facebook.react.bridge.Callback;
 import android.view.accessibility.AccessibilityManager;
@@ -14,57 +14,57 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
 
 public class RNReactNativeCheckAccessibilityModule extends ReactContextBaseJavaModule {
-
-  private final ReactApplicationContext reactContext;
-
-  public RNReactNativeCheckAccessibilityModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    return "RNReactNativeCheckAccessibility";
-  }
-
-  @ReactMethod
-  public void isAccessibilityEnabled(Callback callback) {
-      final AccessibilityManager accessibilityManager = (AccessibilityManager) this.reactContext.getSystemService(Context.ACCESSIBILITY_SERVICE);
-      if (accessibilityManager == null || !accessibilityManager.isEnabled()) {
-          callback.invoke("0", null);
-          return;
-      }
-      callback.invoke("1", null);
-  }
-
-  @ReactMethod
-  public void isVoiceOverRunning(Promise promise) {
-    try {
-      final AccessibilityManager accessibilityManager = (AccessibilityManager) this.reactContext.getSystemService(Context.ACCESSIBILITY_SERVICE);
-      if (accessibilityManager == null || !accessibilityManager.isEnabled() || !accessibilityManager.isTouchExplorationEnabled()) {
-          promise.resolve("0");
-          return;
-      }
-      promise.resolve("1"); 
-    } catch (Exception e) {
-      promise.reject("IS_VOICE_OVER_RUNNING_ERROR", e); 
+  
+    private final ReactApplicationContext reactContext;
+  
+    public RNReactNativeCheckAccessibilityModule(ReactApplicationContext reactContext) {
+      super(reactContext);
+      this.reactContext = reactContext;
     }
-  }
-
-  @ReactMethod
-  void announce(String message) {
-      final AccessibilityManager accessibilityManager = (AccessibilityManager) this.reactContext.getSystemService(Context.ACCESSIBILITY_SERVICE);
-      if (accessibilityManager == null || !accessibilityManager.isEnabled()) {
-          return;
+  
+    @Override
+    public String getName() {
+      return "RNReactNativeCheckAccessibility";
+    }
+  
+    @ReactMethod
+    public void isAccessibilityEnabled(Callback callback) {
+        final AccessibilityManager accessibilityManager = (AccessibilityManager) this.reactContext.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        if (accessibilityManager == null || !accessibilityManager.isEnabled()) {
+            callback.invoke("0", null);
+            return;
+        }
+        callback.invoke("1", null);
+    }
+  
+    @ReactMethod
+    public void isVoiceOverRunning(Promise promise) {
+      try {
+        final AccessibilityManager accessibilityManager = (AccessibilityManager) this.reactContext.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        if (accessibilityManager == null || !accessibilityManager.isEnabled() || !accessibilityManager.isTouchExplorationEnabled()) {
+            promise.resolve("0");
+            return;
+        }
+        promise.resolve("1"); 
+      } catch (Exception e) {
+        promise.reject("IS_VOICE_OVER_RUNNING_ERROR", e); 
       }
-
-      final int eventType = AccessibilityEventCompat.TYPE_ANNOUNCEMENT;
-      final AccessibilityEvent event = AccessibilityEvent.obtain(eventType);
-      event.getText().add(message);
-      event.setClassName(RNReactNativeCheckAccessibilityModule.class.getName());
-      event.setPackageName(this.reactContext.getPackageName());
-
-      accessibilityManager.sendAccessibilityEvent(event);
+    }
+  
+    @ReactMethod
+    void announce(String message) {
+        final AccessibilityManager accessibilityManager = (AccessibilityManager) this.reactContext.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        if (accessibilityManager == null || !accessibilityManager.isEnabled()) {
+            return;
+        }
+  
+        final int eventType = AccessibilityEventCompat.TYPE_ANNOUNCEMENT;
+        final AccessibilityEvent event = AccessibilityEvent.obtain(eventType);
+        event.getText().add(message);
+        event.setClassName(RNReactNativeCheckAccessibilityModule.class.getName());
+        event.setPackageName(this.reactContext.getPackageName());
+  
+        accessibilityManager.sendAccessibilityEvent(event);
+    }
+  
   }
-
-}
